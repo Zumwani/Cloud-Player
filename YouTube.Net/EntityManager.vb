5f48ba0e19f65c4cf2cd1396a032ae92bba7bc99
Zumwani <andreas.ingeholm@hotmail.com>
2015-01-05 21:41:19 +0100

    Removed earlier removed files from repo

--- YouTube.Net/EntityManager.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class EntityManager
-
-    Private Property Videos As New List(Of YouTubeVideo)
-    Private Property Playlists As New List(Of YouTubePlaylist)
-    Private Property Channels As New List(Of YouTubeChannel)
-
-End Class
--- YouTube.Net/YouTubePlaylist.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class YouTubePlaylist
-
-End Class
